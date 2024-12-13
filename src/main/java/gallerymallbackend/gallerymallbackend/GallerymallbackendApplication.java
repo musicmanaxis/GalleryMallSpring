6ae997fbab010664704e4ecfcfcfd9922ec6f9d6
Axis <erlia@naver.com>
2024-12-14 06:21:39 +0900

    1

---
+++ src/main/java/gallerymallbackend/gallerymallbackend/GallerymallbackendApplication.java
@@ -0,0 +1,13 @@
+package gallerymallbackend.gallerymallbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GallerymallbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GallerymallbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gallerymallbackend/gallerymallbackend/GallerymallbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package gallerymallbackend.gallerymallbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GallerymallbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
