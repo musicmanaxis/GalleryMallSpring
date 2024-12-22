47d84b311f8b50aab67f7298586f25911eb55ff3
Axis <erlia@naver.com>
2024-12-23 07:10:01 +0900

    8

--- src/main/java/gallerymallbackend/controller/AccountController.java
+++ src/main/java/gallerymallbackend/controller/AccountController.java
@@ -29,3 +29,14 @@ public class AccountController {
 
   } 
 }
+
+//@RequestBody: 클라이언트가 HTTP 요청 본문(body)에 담아 보낸 데이터를 Java 객체로 변환해주는 역할, email과 password
+//@RequestBody는 JSON 데이터를 Map<String, String> 객체로 변환합니다.
+//(@RequestBody Map<String, String> params): 요청 본문의 데이터를 Map<String, String> 형태로 변환하여  params라는 이름의 변수에 할당하겠다는 것을 의미
+
+//Map은 항상 <Key, Value>의 형태로 두 개의 타입 파라미터를 필요로 합니다.
+//params에는 2개의 key와 value 쌍이 존재하게 됩니다.
+//params = {
+//   "email": "user@example.com",
+//   "password": "password123"
+// };
--- src/main/java/gallerymallbackend/repository/MemberRepository.java
+++ src/main/java/gallerymallbackend/repository/MemberRepository.java
@@ -8,3 +8,6 @@ import gallerymallbackend.entity.Member;
 public interface MemberRepository extends JpaRepository<Member, Integer> {
   Member findByEmailAndPassword(String email, String password);
 }
+//스프링 데이터 JPA의 메서드 이름 규칙에 의해 findByEmailAndPassword()이름을 만든것이다.
+//SELECT * FROM members WHERE email = ? AND password = ?로 스프링이 자동으로 쿼리문을 만들어줌
+//Member findByEmailAndPassword(String email, String password);는 Member 엔터티에서 email과 password 필드를 조건으로 조회하겠다는 선언입니다.
