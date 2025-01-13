016c5ee54ab93e0e8035086c0ce241038ec8e289
Axis <erlia@naver.com>
2025-01-14 07:59:14 +0900

    29

--- src/main/java/gallerymallbackend/controller/CartController.java
+++ src/main/java/gallerymallbackend/controller/CartController.java
@@ -30,7 +30,8 @@ CartRepository cartRepository;
 @Autowired
 ItemRepository itemRepository;
 
-@GetMapping("/api/cart/items")   //특정 사용자 장바구니의 아이템을 조회하는 메서드
+ //특정 사용자 카트의 아이템을 조회하는 메서드에서는 사용자가 보낸 쿠키값으로 처리한다.
+@GetMapping("/api/cart/items")  
 public ResponseEntity getCartItems(@CookieValue(value="token", required=false) String token){
   //@CookieValue: 클라이언트가 요청 시 함께 전송한 쿠키의 값을 읽어와 메서드 파라미터에 할당
   //클라이언트가 보낸 쿠키안에서 이름이 "token"인 값을 읽어와 token 변수에 할당
@@ -45,14 +46,17 @@ public ResponseEntity getCartItems(@CookieValue(value="token", required=false) S
 
 //1.헌재 디비에서 cart테이블에는 memberId와 itemId가 저장되어 있으므로 memberId로 아이템을 찾아야 한다.
 //2.동일한 멤버아이디값으로 여러개일 수 있는 카트객체들을 찾아서 리스트로 반환,
-// *여러개의 cart가 필요한 이유는 한명의 사용자가 여러개의 아이템을 장바구니에 담을 수 있기 때문이다. 동일한 memberId이더라도 여러개의 itemId로 cart객체를 여러개 만듦.
+// *여러개의 cart가 필요한 이유는 한명의 사용자가 여러개의 아이템을 장바구니에 담을 수 있기 때문이다. 
+//  동일한 memberId이더라도 여러개의 itemId로 cart객체를 여러개 만듦.
 //  cartList에 있는 각 Cart 객체에서 itemId 값만 추출하여 새로운 List<Integer> 객체로 변환, toList()는 스트림의 최종연산함수
-//3.ItemRepository에서 itemIdList에 있는 아이템을 찾아서 반환, List<Item> findByIdIn(List<Integer> ids)라고고 itemRepository에 정의되어 있음
+//3.ItemRepository에서 itemIdList에 있는 아이템을 찾아서 반환, 
+//  List<Item> findByIdIn(List<Integer> ids)라고고 itemRepository에 정의되어 있음
 
   return new ResponseEntity<>(items, HttpStatus.OK);
 }
 
-//**장바구니에 아이쳄을 담거나 장바구니에 아이쳄을 삭제하는 방식은 넘어온 itemId와 토큰으로 멤머id를 얻어 카트를 추가하거나 삭제하는 방식으로 쓰인다.
+//**카트에 아이쳄을 담거나 아이쳄을 삭제하는 방식은 
+//넘어온 itemId와 토큰을 이용하여 멤머id를 얻어 카트에 아이템을 추가하거나 삭제하는 방식으로 쓰인다.
 
 @PostMapping("/api/cart/items/{itemId}") //{ItemId}는 변수로 받아들이는 것이다.
   public ResponseEntity pushCartItem(  //장바구니에 아이템을 담는 메서드
